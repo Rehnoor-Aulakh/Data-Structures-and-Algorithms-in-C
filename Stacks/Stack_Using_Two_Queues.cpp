0d804911d197a71971d4a2911eeb276660ecaac0
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2024-09-04 19:31:43 +0530

    Update Stack_Using_Two_Queues.cpp

--- Stacks/Stack_Using_Two_Queues.cpp
+++ Stacks/Stack_Using_Two_Queues.cpp
@@ -1,65 +1,74 @@
-class MyStack {
-public:
-    queue<int> q1;
-    queue<int> q2;
-    MyStack() 
-    {
-        //Create two queues
-        
-    }
-    
-    void push(int x) 
-    {
-        if(q1.empty())
-        {
-            q1.push(x);
-        }
-        else
-        {
-            int temp;
-            //pop all the elements from q1 and push into q2
-            while(!q1.empty())
-            {
-                temp=q1.front();
-                q1.pop();
-                q2.push(temp);
-            }
-            //Then insert the new element in q1
-            q1.push(x);
-            //Pop all the elements from q2 and insert into q1
-            while(!q2.empty())
-            {
-                temp=q2.front();
-                q2.pop();
-                q1.push(temp);
-            }
-        }
-        
-    }
-    
-    int pop() 
-    {
-        int x=q1.front();
-        q1.pop();
-        return x;
-    }
-    
-    int top() 
-    {
-        return q1.front();
-    }
-    
-    bool empty() 
-    {
-        return q1.empty();
-    }
+#include<iostream>
+#include<queue>
+using namespace std;
+
+struct Stack
+{
+    std::queue<int> q1;
+    std::queue<int> q2;
 };
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
+void push(struct Stack *st,int data)
+{
+    if(st->q1.empty())
+    {
+        //Push the data to s2
+        st->q2.push(data);
+    }
+    else
+    {
+        st->q1.push(data);
+    }
+}
+
+int pop(struct Stack *st)
+{
+    int i,size;
+    if(st->q2.empty())
+    {
+        size=st->q1.size();
+        i=0;
+        while(i<size-1)
+        {
+            //Dequeue the elements from queue q1 and enque it in q2 until only one elment remains in q1 which we will return 
+            st->q2.push(st->q1.front());
+            st->q1.pop();
+            i++;
+        }
+        int val=st->q1.front();
+        st->q1.pop();
+        return val;
+    }
+    else
+    {
+        size=st->q2.size();
+        //Push the elements in q1
+        i=0;
+        while(i<size-1)
+        {
+            st->q1.push(st->q2.front());
+            st->q2.pop();
+            i++;
+        }
+        int val=st->q2.front();
+        st->q2.pop();
+        return val;
+    }
+}
+
+int main()
+{
+    struct Stack *st=(struct Stack *)malloc(sizeof(struct Stack));
+    printf("Adding Elements in Stack 10,20,30,40,50\n");
+    push(st,10);
+    push(st,20);
+    push(st,30);
+    push(st,40);
+    push(st,50);
+    printf("Removing Elements from stack\n");
+    printf("%d ",pop(st));
+    printf("%d ",pop(st));
+    printf("%d ",pop(st));
+    printf("%d ",pop(st));
+    printf("%d ",pop(st));
+}
