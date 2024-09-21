50b210a43ee4da6972690b83f61468304775b8b6
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2024-09-21 20:35:46 +0530

    Update q2_Bubble_Sort.c

--- Assignment 2/q2_Bubble_Sort.c
+++ Assignment 2/q2_Bubble_Sort.c
@@ -3,30 +3,32 @@ using namespace std;
 
 int main()
 {
-    int n=7;
-    int i,j,t;
-    int a[]={64,34,25,12,22,11,90};
-    int a[n];
-    for(i=0;i<=n-1;i++)
+    int a[]={10,8,2,7,9,6,1};
+    int size=sizeof(a)/sizeof(int);
+    int i,j;
+    //Printing Initial Array
+    for(i=0;i<size;i++)
     {
-        cout<<"Enter "<<i+1<<" Number";
-        cin>>a[i];
+        cout<<a[i]<<" ";
     }
-    for(i=0;i<=n-2;i++)
+    cout<<endl;
+    //First Loop will be of passes and runs size-1 times
+    for(i=0;i<size-1;i++)
     {
-        for(j=0;j<=(n-2)-i;j++)
+        //Nested Loop for comparing j with j+1 element and if j+1 is smaller, swap it with j
+        for(j=0;j<size-1-i;j++)
         {
-            if(a[j]>a[j+1])
+            if(a[j+1]<a[j])
             {
-                t=a[j];
+                int t=a[j];
                 a[j]=a[j+1];
                 a[j+1]=t;
             }
         }
     }
-    
-    for(i=0;i<=n-1;i++)
+    for(i=0;i<size;i++)
     {
         cout<<a[i]<<" ";
     }
+    cout<<endl;
 }
